03a698311cde5295001f95d39cadc2ddcf198f79
rpg2014 <rpg2014@users.noreply.github.com>
2022-04-04 09:40:44 -0700

    Added wasm build to git
    For real this time

---
+++ steezy-wasm/pkg/steezy_wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function greet(): void;
+/**
+*/
+export class CalcuationEngine {
+  free(): void;
+/**
+* @param {any[]} rules
+* @param {any[]} rule_scalings
+* @param {any[]} seasons
+* @returns {CalcuationEngine}
+*/
+  static new(rules: any[], rule_scalings: any[], seasons: any[]): CalcuationEngine;
+/**
+* @returns {any[]}
+*/
+  get_rule_ids(): any[];
+}
