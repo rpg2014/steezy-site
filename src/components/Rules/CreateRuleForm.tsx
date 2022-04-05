0b0b138f921a7a039da361852714c6987bb5836b
rpg2014 <rpg2014@users.noreply.github.com>
2022-04-04 18:52:26 -0700

    Restored Text + updated auth for rules

--- src/components/Rules/CreateRuleForm.tsx
+++ src/components/Rules/CreateRuleForm.tsx
@@ -27,8 +27,8 @@ interface FormState {
 const initalFormState: FormState = {
     name: "",
     description: "",
-    frequency: Frequency.SEASON, 
-    greenPoints: 0, 
+    frequency: Frequency.SEASON,
+    greenPoints: 0,
     bluePoints: 0,
     blackPoints: 0,
     doubleBlackPoints: 0,
@@ -37,82 +37,84 @@ const initalFormState: FormState = {
 
 // if given ruleid we need to fetch it and prefill out the form.  
 // will need to show error if you dont have permissions?
-export const CreateRuleForm = ({ruleId }: {ruleId?: string | null}) =>  {
+export const CreateRuleForm = ({ ruleId }: { ruleId?: string | null }) => {
     const [formState, setFormState] = useState<FormState>(initalFormState)
 
-    const[loading, setLoading] = useState(ruleId ?true: false);
+    const [loading, setLoading] = useState(ruleId ? true : false);
 
-    const{riderData} = useSignedInRider();
-    const {signedIn, cognitoId} = useAuth();
+    const { riderData } = useSignedInRider();
+    const { signedIn, cognitoId } = useAuth();
 
-    useEffect(()=> {
-        const getRuleToEdit  = async () => {
-            if(ruleId){
+    useEffect(() => {
+        const getRuleToEdit = async () => {
+            if (ruleId) {
                 setLoading(true)
                 const original = await DataStore.query(Rule, ruleId)
-                if(original) {
+                if (original) {
                     setFormState({
                         name: original.name,
                         description: original.description,
-                        frequency: original.frequency as Frequency, 
+                        frequency: original.frequency as Frequency,
                         //@ts-ignore: We verify that green is present, before getting
-                        greenPoints: riderLevelToPointsMap.has(RiderLevels.GREEN) ? original.levelPointsMap[riderLevelToPointsMap.get(RiderLevels.GREEN)]: 0, 
+                        greenPoints: riderLevelToPointsMap.has(RiderLevels.GREEN) ? original.levelPointsMap[riderLevelToPointsMap.get(RiderLevels.GREEN)] : 0,
                         //@ts-ignore: We verify that green is present, before getting
-                        bluePoints: riderLevelToPointsMap.has(RiderLevels.BLUE) ? original.levelPointsMap[riderLevelToPointsMap.get(RiderLevels.BLUE)]: 0, 
+                        bluePoints: riderLevelToPointsMap.has(RiderLevels.BLUE) ? original.levelPointsMap[riderLevelToPointsMap.get(RiderLevels.BLUE)] : 0,
                         //@ts-ignore: We verify that green is present, before getting
-                        blackPoints: riderLevelToPointsMap.has(RiderLevels.BLACK) ? original.levelPointsMap[riderLevelToPointsMap.get(RiderLevels.BLACK)]: 0, 
+                        blackPoints: riderLevelToPointsMap.has(RiderLevels.BLACK) ? original.levelPointsMap[riderLevelToPointsMap.get(RiderLevels.BLACK)] : 0,
                         //@ts-ignore: We verify that green is present, before getting
-                        doubleBlackPoints: riderLevelToPointsMap.has(RiderLevels.DOUBLEBLACK) ? original.levelPointsMap[riderLevelToPointsMap.get(RiderLevels.DOUBLEBLACK)]: 0, 
+                        doubleBlackPoints: riderLevelToPointsMap.has(RiderLevels.DOUBLEBLACK) ? original.levelPointsMap[riderLevelToPointsMap.get(RiderLevels.DOUBLEBLACK)] : 0,
                     })
                     setLoading(false)
-                }       
+                }
             }
         }
         getRuleToEdit();
-    },[ruleId])
-    
+    }, [ruleId])
+
 
     const onSubmit = () => {
-        
-        if(!formState.name || !formState.description || !formState.frequency){
+
+        if (!formState.name || !formState.description || !formState.frequency) {
             throw new Error("Missing a field, double check the form")
-        }else if(formState.greenPoints === 0 && formState.bluePoints === 0 && formState.blackPoints === 0 && formState.doubleBlackPoints){ 
+        } else if (formState.greenPoints === 0 && formState.bluePoints === 0 && formState.blackPoints === 0 && formState.doubleBlackPoints) {
             throw new Error("Cant create rule with points as all 0");
-        }else{
+        } else {
             //TODO: if prop ruleId is present, edit exsisting rule, else new rule
             // Long term todo: if name is same or similar, suggest rule to edit instead, see comment below
-            if(ruleId){
-                
-                return DataStore.query(Rule, ruleId).then((original: Rule| undefined)=> {
-                    if(original ) {
-                    return DataStore.save(Rule.copyOf(original, updated =>{
-                    updated.name = formState.name;
-                    updated.description = formState.description;
-                    updated.frequency = formState.frequency;
-                    updated.lastEditedByCognitoId = cognitoId;
-                    updated.levelPointsMap = JSON.stringify({
-                        greenPoints: formState.greenPoints,
-                        bluePoints: formState.bluePoints,
-                        blackPoints: formState.blackPoints,
-                        doubleBlackPoints: formState.doubleBlackPoints,
-                  })}))// create new rule if one doesn't exist
-                }else {
-                    
-                    return DataStore.save(new Rule({
-                        name: formState.name,
-                        description: formState.description,
-                        frequency: formState.frequency,
-                        lastEditedByCognitoId: cognitoId,
-                        levelPointsMap: JSON.stringify({
-                            greenPoints: formState.greenPoints,
-                            bluePoints: formState.bluePoints,
-                            blackPoints: formState.blackPoints,
-                            doubleBlackPoints: formState.doubleBlackPoints,
-                    })}))
-                }
+            if (ruleId) {
+
+                return DataStore.query(Rule, ruleId).then((original: Rule | undefined) => {
+                    if (original) {
+                        return DataStore.save(Rule.copyOf(original, updated => {
+                            updated.name = formState.name;
+                            updated.description = formState.description;
+                            updated.frequency = formState.frequency;
+                            updated.lastEditedByCognitoId = cognitoId;
+                            updated.levelPointsMap = JSON.stringify({
+                                greenPoints: formState.greenPoints,
+                                bluePoints: formState.bluePoints,
+                                blackPoints: formState.blackPoints,
+                                doubleBlackPoints: formState.doubleBlackPoints,
+                            })
+                        }))// create new rule if one doesn't exist
+                    } else {
+
+                        return DataStore.save(new Rule({
+                            name: formState.name,
+                            description: formState.description,
+                            frequency: formState.frequency,
+                            lastEditedByCognitoId: cognitoId,
+                            levelPointsMap: JSON.stringify({
+                                greenPoints: formState.greenPoints,
+                                bluePoints: formState.bluePoints,
+                                blackPoints: formState.blackPoints,
+                                doubleBlackPoints: formState.doubleBlackPoints,
+                            })
+                        }))
+                    }
                 })
-                  
-            }else{
+
+            } else {
                 return DataStore.save(new Rule({
                     name: formState.name,
                     description: formState.description,
@@ -124,12 +126,12 @@ export const CreateRuleForm = ({ruleId }: {ruleId?: string | null}) =>  {
                         blackPoints: formState.blackPoints,
                         doubleBlackPoints: formState.doubleBlackPoints,
                     })
-                    
-                }), /* Doesn't work yet r=> r.name("eq",formState.name)  want to prevent updating the same rules.*/)
+
+                }))
             }
         }
     }
-    const successPopover = () => toast.success(ruleId ?'Rule Updated!': "Rule Created!",
+    const successPopover = () => toast.success(ruleId ? 'Rule Updated!' : "Rule Created!",
         {
             position: "bottom-center",
             autoClose: 3000,
@@ -141,51 +143,49 @@ export const CreateRuleForm = ({ruleId }: {ruleId?: string | null}) =>  {
             theme: 'dark',
         })
 
-    const {data, error, loading:submitLoading, execute} = useAsyncAction<Rule>(()=>  onSubmit().then(e=>{successPopover(); return e}))
+    const { data, error, loading: submitLoading, execute } = useAsyncAction<Rule>(() => onSubmit().then(e => { successPopover(); return e }))
 
-    if(!signedIn) {
+    if (!signedIn) {
         return <Alert className={styles.errorAlert} variant='danger'>
-        You must sign in to create a rule
-    </Alert>
+            You must sign in to create a rule
+        </Alert>
     }
 
     return (
         <div className={styles.createRuleContainer}>
             {loading ? <Spinner animation='border' />
-            : <>
-            <div className={styles.title}  >
-                {ruleId ? 'Edit Rule' : 'Create Rule'}
-            </div>
-            
-            <div className={styles.seeAllRulesDiv}>
-                Search the  <Link passHref href='/rules'>rule's</Link> to make sure the rule doesn't already exist
-            </div>
-            {/* {!loading ? */}
-             <div className={styles.form}>
-                <InputGroup label='Rule Name' name='name' type='text' setFormState={setFormState} value={formState.name} />
-                <InputGroup label='How do you earn it?' name='description' type='text' setFormState={setFormState} value={formState.description} />
-                <InputGroup label='How Often?' select={{ options: Object.keys(Frequency) }} name='frequency' type='text' setFormState={setFormState} value={formState.frequency} />
-                <h4 className=''>Points</h4>
-                <InputGroup label='Green Rider Points' name='greenPoints' setFormState={setFormState} value={formState.greenPoints.toString()} />
-                <InputGroup label='Blue Rider Points' name='bluePoints' setFormState={setFormState} value={formState.bluePoints.toString()} />
-                <InputGroup label='Black Rider Points' name='blackPoints' setFormState={setFormState} value={formState.blackPoints.toString()} />
-                <InputGroup label='Double Black Rider Points' name='doubleBlackPoints' setFormState={setFormState} value={formState.doubleBlackPoints.toString()} />
+                : <>
+                    <div className={styles.title}  >
+                        {ruleId ? 'Edit Rule' : 'Create Rule'}
+                    </div>
+
+                    <div className={styles.seeAllRulesDiv}>
+                        Search the  <Link passHref href='/rules'>rule's</Link> to make sure the rule doesn't already exist
+                    </div>
+                    {/* {!loading ? */}
+                    <div className={styles.form}>
+                        <InputGroup label='Rule Name' name='name' type='text' setFormState={setFormState} value={formState.name} />
+                        <InputGroup label='How do you earn it?' name='description' type='text' setFormState={setFormState} value={formState.description} />
+                        <InputGroup label='How Often?' select={{ options: Object.keys(Frequency) }} name='frequency' type='text' setFormState={setFormState} value={formState.frequency} />
+                        <h4 className=''>Points</h4>
+                        <InputGroup label='Green Rider Points' name='greenPoints' setFormState={setFormState} value={formState.greenPoints.toString()} />
+                        <InputGroup label='Blue Rider Points' name='bluePoints' setFormState={setFormState} value={formState.bluePoints.toString()} />
+                        <InputGroup label='Black Rider Points' name='blackPoints' setFormState={setFormState} value={formState.blackPoints.toString()} />
+                        <InputGroup label='Double Black Rider Points' name='doubleBlackPoints' setFormState={setFormState} value={formState.doubleBlackPoints.toString()} />
 
 
-                <div className={styles.interactionContainer}>
-                    {submitLoading ? <Spinner animation='border' variant="light" /> :
-                    !data? <Button variant='light' onClick={execute}>Submit</Button>: <Link passHref href={`/rules?ruleId=${data.id}`}><Button variant='outline-light'>See Rule List</Button></Link>}
+                        <div className={styles.interactionContainer}>
+                            {submitLoading ? <Spinner animation='border' variant="light" /> :
+                                !data ? <Button variant='light' onClick={execute}>Submit</Button> : <Link passHref href={`/rules?ruleId=${data.id}`}><Button variant='outline-light'>See Rule List</Button></Link>}
 
-                </div>
-                {error && <Alert className={styles.errorAlert} variant='danger'>
-                    {error.message}
-                </Alert>}
-                {/* {data && <Alert className={styles.successAlert} variant='success'>Success! {ruleId ? 'Edited' :'Created'} rule {data.name}</Alert>} */}
-            </div>
-            </>
-}
-            
-                 {/* <Spinner animation='border' variant="light" /> */}
+                        </div>
+                        {error && <Alert className={styles.errorAlert} variant='danger'>
+                            {error.message}
+                        </Alert>}
+                        {/* {data && <Alert className={styles.successAlert} variant='success'>Success! {ruleId ? 'Edited' :'Created'} rule {data.name}</Alert>} */}
+                    </div>
+                </>
+            }
         </div>
     )
 }
